4dae6b8df2a3032a7c123249729a5eb4bbb5a503
wavepig <2764306748@qq.com>
2024-01-03 23:34:27 +0800

    feat: 基础o开销结构

---
+++ src/first.rs
@@ -0,0 +1,18 @@
+// 单向链表实现栈数据结构
+// List 的尾部不会再分配多余的 junk 值，通过!
+// List 枚举的形式可以享受 null 指针优化，完美！
+// 所有的元素都拥有统一的内存分配.
+pub struct List {
+    head: Link,
+}
+
+enum Link {
+    Empty,
+    // List 不能确定大小需要用 Box 包裹,对于rust编译器来说所有栈上的类型都必须在编译期有固定的长度
+    More(Box<Node>),
+}
+
+struct Node {
+    elem: i32,
+    next: List,
+}
