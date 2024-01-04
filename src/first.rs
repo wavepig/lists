f38240afc8d923920279e13fbff8f54b6761e024
wavepig <2764306748@qq.com>
2024-01-04 23:17:49 +0800

    feat: 实现push

--- src/first.rs
+++ src/first.rs
@@ -2,18 +2,35 @@
 // List 的尾部不会再分配多余的 junk 值，通过!
 // List 枚举的形式可以享受 null 指针优化，完美！
 // 所有的元素都拥有统一的内存分配.
-pub struct List {
-    head: Link,
-}
-
 // Link
+#[derive(Debug, Clone)]
 enum Link {
     Empty,
     // List 不能确定大小需要用 Box 包裹,对于rust编译器来说所有栈上的类型都必须在编译期有固定的长度
     More(Box<Node>),
 }
 
+#[derive(Debug, Clone)]
 struct Node {
     elem: i32,
-    next: List,
+    next: Link,
+}
+
+#[derive(Debug, Clone)]
+pub struct List {
+    head: Link,
+}
+
+impl List {
+    fn new() -> Self {
+        List { head: Link::Empty }
+    }
+    pub fn push(&mut self, elem: i32) {
+        let new_node: Box<Node> = Box::new(Node {
+            elem: elem,
+            // 函数允许我们从一个借用中偷出一个值的同时再放入一个新值。
+            next: std::mem::replace(&mut self.head, Link::Empty),
+        });
+        self.head = Link::More(new_node);
+    }
 }
